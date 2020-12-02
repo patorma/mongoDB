5b38fa1f7dc84e82f1c5f1446ccb4278899b5a7b
patorma <pocontreras@ing.ucsc.cl>
2020-12-01 21:56:20 -0300

    se vio operador diferente e igual

--- sentencias.js
+++ sentencias.js
@@ -38,3 +38,28 @@ db.users.find(
     {age: 25}, // Criterios -> Where
     {age:false} // Select
 ).pretty()
+
+// para buscar valor diferente a se usa la siguiente nomenclatura
+// $ne -> diferente a 
+// $eq -> igual a 
+
+//obtengamos todos los usuarios cuya edad sea diferente de 25 
+
+db.users.find(
+    {
+        age: {
+            $ne: 25
+        }
+    }
+).pretty()
+
+//obtengamos todos los usuarios cuya edad sea igual a 25
+
+db.users.find(
+    {
+        age: {
+            $eq: 25
+        }
+    }
+).pretty()
+
